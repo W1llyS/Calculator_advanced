118da262b2a33f87edd682175548a0bc5481fcb7
HuleJan <hulejan@outlook.com>
2023-08-03 15:43:29 +0200

    First commit

---
+++ Calculator_V2/Calculator_V2.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator_V2
+{
+    public partial class Calculator_V2 : System.Web.UI.Page
+    {
+        public void SendError(Exception ex)
+        {         
+            System.Diagnostics.Debug.WriteLine(ex.ToString()); 
+        }
+
+        // DataLayer instance for data access
+        private DataLayer dataLayer = new DataLayer();
+
+        // ViewState variables to store calculator state
+        protected double resultValue
+        {
+            get { return ViewState["resultValue"] != null ? (double)ViewState["resultValue"] : 0; }
+            set { ViewState["resultValue"] = value; }
+        }
+
+        protected string operationPerformed
+        {
+            get { return ViewState["operationPerformed"] != null ? (string)ViewState["operationPerformed"] : ""; }
+            set { ViewState["operationPerformed"] = value; }
+        }
+
+        protected bool isOperationPerformed
+        {
+            get { return ViewState["isOperationPerformed"] != null ? (bool)ViewState["isOperationPerformed"] : false; }
+            set { ViewState["isOperationPerformed"] = value; }
+        }
+
+        protected bool justCalculated
+        {
+            get { return ViewState["justCalculated"] != null ? (bool)ViewState["justCalculated"] : false; }
+            set { ViewState["justCalculated"] = value; }
+        }     
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadHistory(); // Load and display calculation history on initial page load
+            }
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            if (justCalculated)
+            {
+                TextBoxVysledek.Text = string.Empty;
+                justCalculated = false;
+            }
+
+            if ((TextBoxVysledek.Text == "0") || (isOperationPerformed))
+            {
+                TextBoxVysledek.Text = string.Empty;
+            }
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            TextBoxVysledek.Text = TextBoxVysledek.Text + button.Text; // will show clicked digit on the display
+        }
+
+        protected void Operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Text;  // Store the selected arithmetic operator
+            resultValue = double.Parse(TextBoxVysledek.Text); // Store the first number for the operation
+            isOperationPerformed = true;
+        }
+
+        protected void ButtonC_Click(object sender, EventArgs e)
+        {
+            TextBoxVysledek.Text = "0"; // Clear the display
+            resultValue = 0; // Reset the first number for the next calculation
+        }
+
+        protected void ButtonSpocitat_Click(object sender, EventArgs e)
+        {
+            double secondNumber = double.Parse(TextBoxVysledek.Text);
+            double result = 0;
+
+            // Perform the calculation based on the selected operator
+            switch (operationPerformed)
+            {
+                case "+":
+                    result = resultValue + secondNumber;
+                    break;
+                case "-":
+                    result = resultValue - secondNumber;
+                    break;
+                case "*":
+                    result = resultValue * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                    {
+                        result = resultValue / secondNumber;
+                    }
+                    else
+                    {
+                        
+                        TextBoxVysledek.Text = "Error: Division by zero";
+                        return; // Exit the method without saving the result
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            // Round the result to the closest whole number if checkbox is checked
+            if (CheckBoxWholeNumbers.Checked)
+            {
+                result = Math.Round(result); 
+            }
+
+            // Display the result, save it in the history, and reload the history
+            TextBoxVysledek.Text = result.ToString();
+            dataLayer.SaveResult(resultValue, operationPerformed, secondNumber, result);
+            LoadHistory();
+
+            // Setting the variable for the next calculation
+            justCalculated = true;
+            operationPerformed = "";
+            resultValue = 0;
+            isOperationPerformed = false;
+        }
+
+        // Method to load and display calculation history
+        private void LoadHistory()
+        {
+            try
+            {
+                var history = dataLayer.GetHistory();
+                TextBoxHistory.Text = ""; // Deleting existing content
+                foreach (var record in history)
+                {
+                    TextBoxHistory.Text += $"{record.FirstNumber} {record.Operation} {record.SecondNumber} = {record.Result}\r\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                TextBoxHistory.Text = $"Chyba při načítání historie: {ex.Message}\r\n";
+                SendError(ex); 
+            }
+        }
+    }
+}
---
+++ Calculator_V2/DataLayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+public class DataLayer
+{
+    private string connectionString = "Server=tcp:calculator-server1123.database.windows.net,1433;Initial Catalog=Calculator-database;Persist Security Info=False;User ID=sqladmin;Password=Sqladmi@128;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    // Save the calculation result to the database
+    public void SaveResult(double firstNumber, string operation, double secondNumber, double result)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Results (FirstNumber, Operation, SecondNumber, Result) VALUES (@firstNumber, @operation, @secondNumber, @result)";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@firstNumber", firstNumber);
+                command.Parameters.AddWithValue("@operation", operation);
+                command.Parameters.AddWithValue("@secondNumber", secondNumber);
+                command.Parameters.AddWithValue("@result", result);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // Retrieve the calculation history from the database
+    public List<CalculationRecord> GetHistory()
+    {
+        List<CalculationRecord> history = new List<CalculationRecord>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT TOP 10 FirstNumber, Operation, SecondNumber, Result FROM Results ORDER BY Id DESC";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        CalculationRecord record = new CalculationRecord
+                        {
+                            FirstNumber = (double)reader["FirstNumber"],
+                            Operation = reader["Operation"].ToString(),
+                            SecondNumber = (double)reader["SecondNumber"],
+                            Result = (double)reader["Result"]
+                        };
+                        history.Add(record);
+                    }
+                }
+            }
+        }
+
+        return history; 
+    }
+
+    // Class to represent a single calculation record
+    public class CalculationRecord
+    {
+        public double FirstNumber { get; set; }
+        public string Operation { get; set; }
+        public double SecondNumber { get; set; }
+        public double Result { get; set; }
+    }
+}
