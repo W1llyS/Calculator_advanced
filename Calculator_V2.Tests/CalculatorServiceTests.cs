5724a5d3cce4db8dee440d01b729a0a9bdb26015
Jan H <hulejan@outlook.com>
2025-06-21 20:36:13 +0200

    Add unit tests for CalculatorService and DataLayer

--- Calculator_V2.Tests/CalculatorServiceTests.cs
+++ Calculator_V2.Tests/CalculatorServiceTests.cs
@@ -1,39 +1,71 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Calculator_V2.DataAccess;
-using Calculator_V2.Models;
 using Calculator_V2.Services;
-using System.Collections.Generic;
+using Calculator_V2.DataAccess;
 
 namespace Calculator_V2.Tests
 {
     [TestClass]
-    public class CalculatorServiceTests
+    public class CalculatorServiceLogicTests
     {
-        private Mock<DataLayer> _mockDataLayer;
+        private Mock<IDataAccess> _mockDataAccess;
         private CalculatorService _service;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockDataLayer = new Mock<DataLayer>();
-            _service = new CalculatorService(_mockDataLayer.Object);
+            _mockDataAccess = new Mock<IDataAccess>();
+            _service = new CalculatorService(_mockDataAccess.Object);
         }
 
         [TestMethod]
-        public void Compute_Addition_ReturnsCorrectResult()
+        public void Compute_Addition_ReturnsSum()
         {
-            double result = _service.Calculate(5, 3, "+", false);
-            Assert.AreEqual(8, result);
+            double result = _service.Calculate(10, 5, "+", false);
+            Assert.AreEqual(15, result);
         }
 
         [TestMethod]
-        public void Compute_Division_RoundsCorrectly()
+        public void Compute_Subtraction_ReturnsDifference()
         {
-            double result = _service.Calculate(7, 2, "/", true);
-            Assert.AreEqual(4, result); // 7/2 = 3.5 → rounded to 4
+            double result = _service.Calculate(10, 5, "-", false);
+            Assert.AreEqual(5, result);
         }
 
-       
+        [TestMethod]
+        public void Compute_Multiplication_ReturnsProduct()
+        {
+            double result = _service.Calculate(4, 3, "*", false);
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void Compute_Division_ReturnsQuotient()
+        {
+            double result = _service.Calculate(10, 2, "/", false);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Compute_Division_RoundsResult()
+        {
+            double result = _service.Calculate(7, 2, "/", true); // 3.5 → 4
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void Compute_DivisionByZero_ThrowsException()
+        {
+            _service.Calculate(5, 0, "/", false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void Compute_InvalidOperator_ThrowsException()
+        {
+            _service.Calculate(5, 3, "%", false);
+        }
     }
 }
+
---
+++ Calculator_V2.Tests/DataLayerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator_V2.DataAccess;
+using System;
+
+namespace Calculator_V2.Tests
+{
+    [TestClass]
+    public class DataLayerTests
+    {
+        private DataLayer _dataLayer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _dataLayer = new DataLayer();
+        }
+
+        [TestMethod]
+        public void SaveResult_DoesNotThrow()
+        {
+            var ex = Record.Exception(() =>
+                _dataLayer.SaveResult(2, "+", 3, 5));
+
+            Assert.IsNull(ex);
+        }
+
+        [TestMethod]
+        public void GetHistory_ReturnsList()
+        {
+            var history = _dataLayer.GetHistory();
+            Assert.IsNotNull(history);
+        }
+    }
+
+    public static class Record
+    {
+        public static Exception Exception(Action action)
+        {
+            try
+            {
+                action();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+    }
+}
