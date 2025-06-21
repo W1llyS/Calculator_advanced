4a306ffc944c4aefb4ecce1eaa3948f630c67f77
Jan H <hulejan@outlook.com>
2025-06-21 21:04:10 +0200

    Refactor DataLayer to use Dapper

--- Calculator_V2/DataAccess/DataLayer.cs
+++ Calculator_V2/DataAccess/DataLayer.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data.SqlClient;
+using Dapper;
 using Calculator_V2.Models;
-
 
 namespace Calculator_V2.DataAccess
 {
@@ -9,56 +9,37 @@ namespace Calculator_V2.DataAccess
     {
         private string connectionString = "workstation id=SachyDB.mssql.somee.com;packet size=4096;user id=sachyckyne_SQLLogin_1;pwd=ri31z9jhqv;data source=SachyDB.mssql.somee.com;persist security info=False;initial catalog=SachyDB;TrustServerCertificate=True";
 
-        // Save the calculation result to the database
         public void SaveResult(double firstNumber, string operation, double secondNumber, double result)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "INSERT INTO Results (FirstNumber, Operation, SecondNumber, Result) VALUES (@firstNumber, @operation, @secondNumber, @result)";
-                using (SqlCommand command = new SqlCommand(query, connection))
+                const string sql = @"
+                    INSERT INTO Results (FirstNumber, Operation, SecondNumber, Result) 
+                    VALUES (@FirstNumber, @Operation, @SecondNumber, @Result)";
+                connection.Execute(sql, new
                 {
-                    command.Parameters.AddWithValue("@firstNumber", firstNumber);
-                    command.Parameters.AddWithValue("@operation", operation);
-                    command.Parameters.AddWithValue("@secondNumber", secondNumber);
-                    command.Parameters.AddWithValue("@result", result);
-                    command.ExecuteNonQuery();
-                }
+                    FirstNumber = firstNumber,
+                    Operation = operation,
+                    SecondNumber = secondNumber,
+                    Result = result
+                });
             }
         }
 
-        // Retrieve the calculation history from the database
         public List<CalculationRecord> GetHistory()
         {
-            List<CalculationRecord> history = new List<CalculationRecord>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "SELECT TOP 10 FirstNumber, Operation, SecondNumber, Result FROM Results ORDER BY Id DESC";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            CalculationRecord record = new CalculationRecord
-                            {
-                                FirstNumber = (double)reader["FirstNumber"],
-                                Operation = reader["Operation"].ToString(),
-                                SecondNumber = (double)reader["SecondNumber"],
-                                Result = (double)reader["Result"]
-                            };
-                            history.Add(record);
-                        }
-                    }
-                }
+                const string sql = @"
+                    SELECT TOP 10 FirstNumber, Operation, SecondNumber, Result 
+                    FROM Results 
+                    ORDER BY Id DESC";
+                return connection
+                    .Query<CalculationRecord>(sql)
+                    .AsList();
             }
-
-            return history;
         }
-
-        // Class to represent a single calculation record
-
     }
 }
